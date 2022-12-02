f297f926b55e0fa55f929b5562e2dadb498715b3
PETISOLUCIONES <ingenieria@peti.com.co>
2022-12-02 14:58:27 -0500

    legal

---
+++ l10n_co_legal_reports/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from . import legal_reports_tax
+from . import legal_reports_partner
+from . import legal_reports_account
+from . import legal_tax_reports_data
+from . import legal_taxt_report_data_lines
---
+++ l10n_co_legal_reports/models/__init__.pye
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from . import legal_reports_tax
+from . import legal_reports_partner
+from . import legal_reports_account
